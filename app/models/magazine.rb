d64fb2d90115b19c26f99ba45bea81935089d6e5
jacob <jacobharv00@gmail.com>
2021-07-27 17:28:47 -0600

    Done

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,3 +1,16 @@
 class Magazine < ActiveRecord::Base
-  
+  has_many :subscriptions, dependent: :destroy
+  has_many :readers, through: :subscriptions
+
+
+  def email_list
+    self.readers.map {|person| person.email}.join(";")
+    # readers.pluck(:email).join(";") same but better than line 7
+  end
+
+  def self.most_popular
+    self.all.max_by {|mag| mag.subscriptions.length}
+    # includes(:subscriptions).all.max_by {|mag| mag.subscriptions.length} only runs two querys and better than line 12
+  end
+
 end
--- app/models/reader.rb
+++ app/models/reader.rb
@@ -1,3 +1,18 @@
 class Reader < ActiveRecord::Base
+  has_many :subscriptions, dependent: :destroy
+  has_many :magazines, through: :subscriptions
+
+  def subscribe magazine, price
+    Subscription.create(magazine_id: magazine.id, reader_id: self.id, price: price)
+  end
   
+  def total_subscription_price
+    self.subscriptions.select{|subscription| subscription.price}.sum
+    # subscriptions.sum(:price) same as line 10 but better
+  end
+
+  def cancel_subscription magazine
+    subscription.find_by(magazine_id: magazine.id).destroy
+  end
+
 end
